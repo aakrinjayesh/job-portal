55c1c6e2640b9a92014b47138f2b1afeb69806d1
Danamma94 <shruthi.danamma@aakrin.com>
2026-05-15 12:31:40 +0530

    shruti social-server file changed

--- social-backend/src/server.js
+++ social-backend/src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { authenticateToken } from "./Middleware/authMiddleware.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import feedRoutes from "./routes/feedRoutes.js";
 import prisma from "./config/prisma.js";
+import userRoutes from "./routes/userRoutes.js";
+import followRoutes from "./routes/followRoutes.js";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -14,6 +22,7 @@ app.use(
   cors({ origin: process.env.SOCIAL_CORS_ORIGIN || "*", credentials: true }),
 );
 app.use(express.json());
+// app.use("/uploads", express.static(join(__dirname, "../uploads")));
 app.use(authenticateToken);
 
 app.get("/health", async (_req, res) => {
@@ -23,6 +32,9 @@ app.get("/health", async (_req, res) => {
 
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/feed", feedRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api",followRoutes);
 
 const PORT = process.env.SOCIAL_PORT || 3003;
 
